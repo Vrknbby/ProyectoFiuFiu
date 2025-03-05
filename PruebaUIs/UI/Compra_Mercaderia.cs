7b3287ba2080749bd1c8c47279d045cd5144d09b
Vrknbby <barrantesnicolas27@gmail.com>
2025-03-05 17:12:28 -0500

    Agregado Cabecera Canje

---
+++ PruebaUIs/UI/Compra_Mercaderia.cs
@@ -0,0 +1,27 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaUIs
+{
+    public partial class Compra_Mercaderia : MaterialForm
+    {
+        public Compra_Mercaderia()
+        {
+            InitializeComponent();
+        }
+
+        private void Compra_Mercaderia_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
